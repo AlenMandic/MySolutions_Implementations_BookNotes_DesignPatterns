7b6326957bcea7d450d7cfd5aff08ea40c017370
Alen <alenmandic1@gmail.com>
2025-11-04 13:57:17 +0100

    Book Notes, and Singleton in C#

---
+++ DesignPatterns/Singleton Pattern - C#/Program.cs
@@ -0,0 +1,41 @@
+﻿using SimpleAsyncTaskExample;
+using SimpleAsyncTaskExample.AbstractClasses_Interfaces;
+using SimpleAsyncTaskExample.TestingFolder1;
+
+class Program
+{    
+    public static async Task<int> GetWebPageLengthAsync(string endpoint)
+    {
+        var client = new HttpClient();
+        var uri = new Uri(endpoint);
+
+        byte[] WebPageContent = await client.GetByteArrayAsync(uri);
+        return WebPageContent.Length;
+
+    }
+
+    public static async Task OutputWebPageLengthAsync()
+    {
+
+        int getWebsiteBytes = await GetWebPageLengthAsync("https://github.com/alenmandic");
+
+        Console.WriteLine("Length of my personal Github in bytes: " + getWebsiteBytes.ToString());
+
+    }
+
+        static void Main(string[] args)
+    {
+        var singleton1 = SingletonPattern.Instance;
+        var singleton2 = SingletonPattern.Instance;
+        var singleton3 = SingletonPattern.Instance;
+
+        // Outputs the same random number for each singleton variable.
+        singleton1.ReturnRandomNumber();
+        singleton2.ReturnRandomNumber();
+        singleton3.ReturnRandomNumber();
+
+        Console.WriteLine(Object.ReferenceEquals(singleton1, singleton2)); // TRUE
+        Console.WriteLine(Object.ReferenceEquals(singleton2, singleton3)); // TRUE
+
+    }
+}
---
+++ DesignPatterns/Singleton Pattern - C#/SingletonPattern.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ THE SINGLETON PATTERN:
+Used to ensure a class can only be instantied once, with a global point of access to it. Useful for sharing persistent state.
+
+IMPORTANT NOTE: Static fields and methods belong to the class itself, not object instances of the class. So when we do static SingletonPattern _instance, _instance is the same for every single SingletonPattern class.
+ */
+
+namespace SimpleAsyncTaskExample
+{
+    internal class SingletonPattern
+    {
+        private static SingletonPattern _instance; // Property which holds the main object reference, of which only 1 should exist
+
+        private Random _random;
+        private int _randomNumber;
+
+        // initialize data we might need inside the constructor. In this case a random number to demonstrate the pattern.
+        // private SingletonPattern() { } // Private constructor ensures new object instances cannot be instantiated.
+        private SingletonPattern()
+        {
+            _random = new Random();
+            _randomNumber = GenerateRandomNumber();
+        }
+
+        public int GenerateRandomNumber()
+        {
+            return _random.Next(10000);
+        }
+
+        public int ReturnRandomNumber()
+        {
+            Console.WriteLine($"Our shared random number is: {_randomNumber}");
+            return _randomNumber;
+        }
+
+        // Property creates the first instance, and returns the instance. Only has a getter, to retrieve the instance.
+        public static SingletonPattern Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new SingletonPattern(); // Initial main instance
+                return _instance;
+            }
+        }
+
+    }
+}
